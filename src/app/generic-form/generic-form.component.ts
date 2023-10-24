11832e4a08479b49c12ea270b5d1674742c8e86e
Izzet Can Sen <izzet.sen@student.ehb.be>
2023-10-24 10:59:30 +0200

    Added GenericForm & SignIn components

---
+++ src/app/generic-form/generic-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-generic-form',
+  templateUrl: './generic-form.component.html',
+  styleUrls: ['./generic-form.component.scss'],
+})
+export class GenericFormComponent {
+  @Input() fields!: any[];
+  @Input() title!: string;
+  @Output() onSubmit = new EventEmitter<void>();
+}
