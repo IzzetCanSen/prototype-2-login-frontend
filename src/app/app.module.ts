0a2adbf91e6994b7b6b7f172875196d4fb635cc3
Izzet Can Sen <izzet.sen@student.ehb.be>
2023-11-12 16:37:54 +0100

    Routing configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GenericFormComponent } from './generic-form/generic-form.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    GenericFormComponent,
-    SignInComponent,
-    SignUpComponent,
-  ],
+  declarations: [AppComponent, SignInComponent, SignUpComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
