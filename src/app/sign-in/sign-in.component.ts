e4c7c9b4974b1f55dd5dc6975d769ddd40d60620
Izzet Can Sen <izzet.sen@student.ehb.be>
2023-11-12 16:21:53 +0100

    Updated Sign-up component

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -23,6 +23,7 @@ export class SignInComponent {
       this.auth.signIn(this.signinForm.value).subscribe({
         next: (res) => {
           alert(res.message);
+          this.signinForm.reset();
         },
         error: (err) => {
           alert(err?.error.message);
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,15 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent {
-  signUpFields = [
-    { name: 'name', label: 'Name', type: 'text', value: '' },
-    { name: 'email', label: 'Email', type: 'email', value: '' },
-    { name: 'password', label: 'Password', type: 'password', value: '' },
-  ];
-  title = 'Sign Up';
-  extraInformation = 'Already have an account? Sign Up';
+  constructor(private auth: AuthService, private fb: FormBuilder) {}
+  signupForm!: FormGroup;
 
-  signUpSubmit() {
-    console.log('submit button pressed');
+  ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      console.log(this.signupForm.value);
+      this.auth.signUp(this.signupForm.value).subscribe({
+        next: (res) => {
+          alert(res.message);
+          this.signupForm.reset();
+        },
+        error: (err) => {
+          alert(err?.error.message);
+        },
+      });
+    } else {
+      console.log('not valid');
+    }
   }
 }
