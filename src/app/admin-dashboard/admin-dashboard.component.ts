f24c9a473f6878b0aa6425726d2804b0c5d91cee
Izzet Can Sen <izzet.sen@student.ehb.be>
2023-11-13 03:50:29 +0100

    Added custom CSS for dashboard & adminDashboard

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.scss']
+  styleUrls: ['./admin-dashboard.component.scss'],
 })
 export class AdminDashboardComponent {
-
+  constructor(private auth: AuthService) {}
+  signOut() {
+    this.auth.signOut();
+  }
 }
